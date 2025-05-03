b3f1c77e7b93ee269d9decbd5917f7c865c0c55a
Badbloodd <dmitriy26496@gmail.com>
2025-05-03 20:53:01 +0300

    first commit

---
+++ myproject/users/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
